d3ad249a4d5f8a3297582e53715af97a926a269b
Tobias Kristoffer Sørensen <Tobias_sorensen@outlook.dk>
2021-04-12 17:43:20 +0200

    installed npm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,19 @@
+<!DOCTYPE html>
 <html>
-    <body>
-        <h1>Hello Welcome to my portfolio page </h1>
 
-        <h1>Please click the button below to see an example of input handeling" </h1>
-        <button>Login</button>
+    <div class="header">
+
+        <h1>Header</h1>
+        <p>My supercool header</p>
+        
+    </div> 
+
+    <body>
+
+        <h1>Hello Welcome to my portfolio page! </h1>
+
+        <h1>Please click the button below to see an example of input handling" </h1>
+        <button>Input handling</button>
 
     </body>
 </html>
