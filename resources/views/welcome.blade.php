42b9aa53878b92f89bb10e9c8c9f5a06ddf4ce15
Tobias Kristoffer Sørensen <Tobias_sorensen@outlook.dk>
2021-04-13 18:24:46 +0200

    Created main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
+    
 <html>
 
-    <div class="header">
+    <body style ="background-color:#7ea3a3"> 
+        <center>
+            <h1 style="color:blue;font-family:Serif;">Hello Welcome to my portfolio page! </h1>
 
-        <h1>Header</h1>
-        <p>My supercool header</p>
-        
-    </div> 
+            <h1>Please click the button below to see an example of input handling </h1>
 
-    <body>
-
-        <h1>Hello Welcome to my portfolio page! </h1>
-
-        <h1>Please click the button below to see an example of input handling" </h1>
-        <button>Input handling</button>
-
+            <button type="button" onclick="window.location='{{ route("InputHandling") }}'" style = "background-color:brown; color:black; padding: 32px 80px;">Button</button>
+        </center>
     </body>
+
 </html>
