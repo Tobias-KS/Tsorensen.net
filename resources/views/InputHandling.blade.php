a66aeea259289d61ef785ccffd6b4deb98eb49a1
Tobias Kristoffer Sørensen <Tobias_sorensen@outlook.dk>
2021-04-15 14:27:06 +0200

    Updated colors on the website

--- resources/views/InputHandling.blade.php
+++ resources/views/InputHandling.blade.php
@@ -1,5 +1,5 @@
 <?php $firstIntro = "Hello please enter your name"; ?>
-<?php $Name = " "?>
+<?php $Name = " "; ?>
 <?php $SecondIntro = "Hello $Name its nice to meet you!"; ?>
 <?php $UseThisIntro = "Hello please enter your name"; ?>
 <?php if ($Name == " ") {
@@ -10,13 +10,14 @@ else {
 } ?>
 <!DOCTYPE html>
 <html>
-<button type="Return " onclick="window.location='{{ route("welcome") }}'" style = "background-color:brown; color:black; padding: 20px 30px;">Home</button>
-    <body style ="background-color:#7ea3a3"> 
+<button type="Return " onclick="window.location='{{ route("welcome") }}'" style = "background-color:#FFF447; color:#1A1A1A; padding: 20px 30px;">Home</button>
+    <body style ="background-color:#696969"> 
         <center>
 
-            <h1 style="color:blue;font-family:Serif;"> {{ $UseThisIntro }}</h1>
-            <label> Your name here </label>
+            <h1 style="color:#3EB595;font-family:Serif;"> {{ $UseThisIntro }}</h1>
+            <label style="color:#3EB595";> Your name here </label>
             <input type = "text" id="name" name = name> 
+            <button style = "background-color:#FFF447;color:#1A1A1A;"> submit </button>
         </center>
     </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 
 <html>
-<h2> For inquiries please contact me at Tobias@tsorensen.net 
-    <body style ="background-color:#7ea3a3"> 
+<h2 style="color:#3EB595"; > For inquiries please contact me at Tobias@tsorensen.net </h2>
+    <body style ="background-color:#696969"> 
         <center>
-            <h1 style="color:blue;font-family:Serif;">Hello Welcome to my portfolio page! </h1>
+            <h1 style="color:#3EB595;font-family:Serif;">Welcome to my portfolio page! </h1>
 
-            <h1>Please click the button below to see an example of input handling </h1>
+            <h1 style="color:#3EB595 ">Please click the button below to see an example of input handling </h1>
 
-            <button type="button" onclick="window.location='{{ route("InputHandling") }}'" style = "background-color:brown; color:black; padding: 32px 80px;">Input handling</button>
+            <button type="button" onclick="window.location='{{ route("InputHandling") }}'" style = "background-color:#FFF447; color:#1A1A1A; padding: 32px 80px;">Input handling</button>
         </center>
     </body>
 
